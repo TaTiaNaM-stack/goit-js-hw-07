ac8077999a40b863e71b35dd423c1951fc93fd3e
TaTiaNaM <maluchenko.tania@gmail.com>
2025-12-07 19:17:28 +0200

    hw

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('.login-form');
 form.addEventListener('submit', event => {
     event.preventDefault();
-    if (form.email.value === '' || form.password.value === '') {
+    if (form.elements.email.value === '' || form.elements.password.value === '') {
         alert('All form fields must be filled in');
     } else {
         const formData = {
-            email: form.email.value.trim(),
-            password: form.password.value.trim(),
+            email: form.elements.email.value.trim(),
+            password: form.elements.password.value.trim(),
         };
         console.log(formData);
         event.currentTarget.reset();
